bb7117f3415b6acf31cafa2c0f29a643575c97e5
dziugaspeciulevicius <dziugas.peciulevicius@visma.com>
2021-06-23 13:49:09 +0300

    navbar w/ theme switcher buttons + theme switcher service ( functionality + local storage )

---
+++ src/app/core/theme-switcher.service.ts
@@ -0,0 +1,43 @@
+import { Injectable, PACKAGE_ROOT_URL } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+enum Theme {
+  Light = 'light-theme',
+  Dark = 'dark-theme',
+  Orange = 'orange-theme',
+}
+@Injectable({
+  providedIn: 'root',
+})
+export class ThemeSwitcherService {
+  private theme$ = new BehaviorSubject<Theme>(this.getInitialTheme());
+
+  constructor() {
+    this.theme$.subscribe((nextTheme) => {
+      document.body.classList.remove(Theme.Light);
+      document.body.classList.remove(Theme.Dark);
+      document.body.classList.remove(Theme.Orange);
+      document.body.classList.add(nextTheme);
+    });
+  }
+
+  enableLightTheme() {
+    this.theme$.next(Theme.Light);
+    localStorage.setItem('theme', 'light-theme');
+  }
+  enableDarkTheme() {
+    this.theme$.next(Theme.Dark);
+    localStorage.setItem('theme', 'dark-theme');
+  }
+  enableOrangeTheme() {
+    this.theme$.next(Theme.Orange);
+    localStorage.setItem('theme', 'orange-theme');
+  }
+
+  private getInitialTheme(): Theme {
+    const storedLocally = localStorage.getItem('theme');
+    let stored: Theme = storedLocally as Theme;
+
+    return stored || Theme.Light;
+  }
+}
